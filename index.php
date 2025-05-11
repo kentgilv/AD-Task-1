948c3935fe2a1dedd5915df917d305ef6356115b
kentgilv <kent.v.valencia@gmail.com>
2025-05-11 21:52:54 +0800

    feat(landing-page): added navigational links

--- index.php
+++ index.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NutriHealth</title>
 </head>
+
 <body>
     <header>
         <nav>
             <div class="nav-brand">
-                <a href="index.php" title="NutriHealth Logo"><img class="brand-logo" src="./assets/img/nutrihealth-logo.png" height="40px" alt="NutriHealth Logo"></a>
+                <a href="index.php" title="NutriHealth Logo"><img class="brand-logo"
+                        src="./assets/img/nutrihealth-logo.png" height="40px" alt="NutriHealth Logo"></a>
                 <a class="brand-name" href="index.php" title="Brand Name">NutriHealth</a>
             </div>
             <div class="toggle-menu">
-                <button id="toggleMenuBtn" class="toggle-menu-btn" onclick="toggleMenu()" title="Show Menu"><span id="toggleIcon" class="material-icons"> menu </span></button>
+                <button id="toggleMenuBtn" class="toggle-menu-btn" onclick="toggleMenu()" title="Show Menu"><span
+                        id="toggleIcon" class="material-icons"> menu </span></button>
             </div>
             <div>
-                <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+                <ul id="navbarLinks" class="navbar-links">
+                    <li><a class="active-link" href="index.php" title="View Homepage">Home</a></li>
+                    <li><a href="" title="View Services">Services</a></li>
+                    <li><a href="" title="View About Information">About</a></li>
+                    <li><a href="" title="View Contact Information">Contact</a></li>
+                    <li><a id="signUpBtnLink" href="" title="Create New Account">Sign Up</a></li>
                 </ul>
             </div>
         </nav>
     </header>
 </body>
+
 </html>
