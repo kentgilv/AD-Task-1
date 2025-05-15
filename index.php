51c17a01660ed4ebea8af60179cb8ad36653184b
kentgilv <kent.v.valencia@gmail.com>
2025-05-15 13:13:06 +0800

    feat(landing-page): added services-page link

--- index.php
+++ index.php
@@ -39,7 +39,8 @@
             <div>
                 <ul id="navLinks" class="nav-links">
                     <li><a class="active-link" href="index.php" title="View Homepage">Home</a></li>
-                    <li><a href="" title="View Services">Services</a></li>
+                    <li><a href="./pages/services-page/index.php
+                    " title="View Services">Services</a></li>
                     <li><a href="" title="View About Information">About</a></li>
                     <li><a href="" title="View Contact Information">Contact</a></li>
                     <li><a id="signUpBtn" href="" title="Create New Account">Sign Up</a></li>
